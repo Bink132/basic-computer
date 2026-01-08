d32b537b00e1b928bd8d7b02a57b4db8bc906190
Bink <2207723@talnet.nl>
2026-01-08 22:21:20 +0100

    Deploy Nuxt 4 static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  ssr: false,
+
+  app: {
+    baseURL: '/basic-computer/',
+  },
+
+  nitro: {
+    preset: 'static',
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
